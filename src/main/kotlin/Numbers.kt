8c1165034a3b4548ec83125ae1f9fffb4c82db7e
alex <alex.pajaron@vier.ai>
2022-09-04 21:26:27 +0200

    range in range

--- src/main/kotlin/Numbers.kt
+++ src/main/kotlin/Numbers.kt
@@ -1,6 +1,9 @@
 class Numbers {
-
     fun filterOdd(list: List<Int>): List<Int> {
         return list.filter { it % 2 == 1 }
     }
+
+    fun containsRange(range1: IntRange, range2: IntRange) =
+        range2.first >= range1.first && range2.last <= range1.last
+
 }
--- src/test/kotlin/NumbersTest.kt
+++ src/test/kotlin/NumbersTest.kt
@@ -1,16 +1,27 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFails
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class NumbersTest {
+internal class NumbersTest {
+
+    private val numbers: Numbers = Numbers()
 
     @Test
     fun testGetOddNumbers(){
-        val numbers = Numbers()
         var result = numbers.filterOdd(listOf(1, 2, 3))
         assertEquals(result, listOf(1, 3))
 
         result = numbers.filterOdd(listOf(4, 6, 8, 7, 9))
         assertEquals(result, listOf(7, 9))
+    }
+
+    @Test
+    fun testRangeContainsRange(){
+        assertTrue(numbers.containsRange(5..7, 5..7))
+        assertTrue(numbers.containsRange(1..12, 5..7))
+        assertFalse(numbers.containsRange(5..8, 5..9))
     }
 
 }
